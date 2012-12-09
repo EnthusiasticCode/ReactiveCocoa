1de67ab10d94b663228c077181162c9386279498
Nicola Peduzzi <thenikso@gmail.com>
2012-12-09 15:42:15 +0100

    Restored previous implementation to avoid more race conditions

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACBindings.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACBindings.m
@@ -23,52 +23,56 @@
 	__block uint32_t otherObjectVersion = 0;
 	__block uint32_t receiverExpectedBounces = 0;
 	__block uint32_t otherObjectExpectedBounces = 0;
-
-	id receiverObserverIdentifier = [self rac_addObserver:otherObject forKeyPath:receiverKeyPath options:0 queue:nil block:^(id observer, NSDictionary *change) {
-		[otherScheduler schedule:^{
-			id value;
-			
-			@synchronized (countersLock) {
-				BOOL shouldSend = receiverVersion - otherObjectVersion < UINT32_MAX / 2;
-				if (receiverExpectedBounces > 0) {
-					receiverExpectedBounces -= 1;
-					receiverVersion += 1;
-				}
-				
-				if (!shouldSend) return;
-				
+	
+	id receiverObserverIdentifier = [self rac_addObserver:otherObject forKeyPath:receiverKeyPath options:NSKeyValueObservingOptionNew queue:nil block:^(id observer, NSDictionary *change) {
+		id value;
+		
+		@synchronized (countersLock) {
+			BOOL shouldSend = receiverVersion - otherObjectVersion < UINT32_MAX / 2;
+			if (receiverExpectedBounces > 0) {
+				receiverExpectedBounces -= 1;
 				receiverVersion += 1;
-				value = [self valueForKeyPath:receiverKeyPath];
-				if (receiverTransformer != nil) value = receiverTransformer(value);
-				
-				otherObjectExpectedBounces += 1;
 			}
 			
-			[otherObject setValue:value forKeyPath:otherKeyPath];
+			if (!shouldSend) return;
+			receiverVersion += 1;
+			value = change[NSKeyValueChangeNewKey]; //[self valueForKeyPath:receiverKeyPath];
+			if (value == [NSNull null]) value = nil;
+		}
+		
+		if (receiverTransformer != nil) value = receiverTransformer(value);
+		
+		[otherScheduler schedule:^{
+			@synchronized (countersLock) {
+				otherObjectExpectedBounces += 1;
+				[otherObject setValue:value forKeyPath:otherKeyPath];
+			}
 		}];
 	}];
 	
-	id otherObjectObserverIdentifier = [otherObject rac_addObserver:self forKeyPath:otherKeyPath options:NSKeyValueObservingOptionInitial queue:nil block:^(id observer, NSDictionary *change) {
-		[receiverScheduler schedule:^{
-			id value;
-			
-			@synchronized (countersLock) {
-				BOOL shouldSend = otherObjectVersion - receiverVersion < UINT32_MAX / 2;
-				if (otherObjectExpectedBounces > 0) {
-					otherObjectExpectedBounces -= 1;
-					otherObjectVersion += 1;
-				}
-				
-				if (!shouldSend) return;
-				
+	id otherObjectObserverIdentifier = [otherObject rac_addObserver:self forKeyPath:otherKeyPath options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionInitial queue:nil block:^(id observer, NSDictionary *change) {
+		id value;
+		
+		@synchronized (countersLock) {
+			BOOL shouldSend = otherObjectVersion - receiverVersion < UINT32_MAX / 2;
+			if (otherObjectExpectedBounces > 0) {
+				otherObjectExpectedBounces -= 1;
 				otherObjectVersion += 1;
-				value = [otherObject valueForKeyPath:otherKeyPath];
-				if (otherTransformer != nil) value = otherTransformer(value);
-				
-				receiverExpectedBounces += 1;
 			}
 			
-			[self setValue:value forKeyPath:receiverKeyPath];
+			if (!shouldSend) return;
+			otherObjectVersion += 1;
+			value = change[NSKeyValueChangeNewKey]; //[otherObject valueForKeyPath:otherKeyPath];
+			if (value == [NSNull null]) value = nil;
+		}
+		
+		if (otherTransformer != nil) value = otherTransformer(value);
+		
+		[receiverScheduler schedule:^{
+			@synchronized (countersLock) {
+				receiverExpectedBounces += 1;
+				[self setValue:value forKeyPath:receiverKeyPath];
+			}
 		}];
 	}];
 	
