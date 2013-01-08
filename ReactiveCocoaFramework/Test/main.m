7efc10cd97301ccde5722d00f87a84d4555f45c3
Uri Baghin <skerfat@gmail.com>
2013-01-08 23:58:35 +0100

    Changed enumerator to ensure flat memory usage.

--- ReactiveCocoaFramework/Test/main.m
+++ ReactiveCocoaFramework/Test/main.m
@@ -9,24 +9,45 @@
 #import <Foundation/Foundation.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
+@interface CountUpToXEnumerator : NSEnumerator
+
++ (instancetype)countUpTo:(NSUInteger)x;
+
+@end
+
+@implementation CountUpToXEnumerator {
+	NSUInteger _x;
+	NSUInteger _current;
+}
+
++ (instancetype)countUpTo:(NSUInteger)x {
+	CountUpToXEnumerator *enumerator = [[self alloc] init];
+	enumerator->_x = x;
+	return enumerator;
+}
+
+- (id)nextObject {
+	if (_current == _x) return nil;
+	return @(_current++);
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
+	
 	@autoreleasepool {
-	    
-		NSUInteger length = 1000000;
-		NSMutableArray *values = [NSMutableArray arrayWithCapacity:length];
-		for (NSUInteger i = 0; i < length; ++i) {
-			[values addObject:@(i)];
-		}
 		
 		__block NSUInteger sum = 0;
-		[values.objectEnumerator.rac_signal subscribeNext:^(NSNumber *x) {
+		[[CountUpToXEnumerator countUpTo:1000000].rac_signal subscribeNext:^(NSNumber *x) {
 			sum += x.unsignedIntegerValue;
 		} completed:^{
 			NSLog(@"%@", @(sum));
+			exit(0);
 		}];
 		
-		sleep(UINT32_MAX);
 	}
+	
+	sleep(UINT32_MAX);
 	return 0;
 }
 
