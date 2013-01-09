9900cacbc185dca18b90751dd0c14a8ff6e6f086
Uri Baghin <skerfat@gmail.com>
2013-01-09 12:56:39 +0100

    Added filter to test sequence.

--- ReactiveCocoaFramework/Test/main.m
+++ ReactiveCocoaFramework/Test/main.m
@@ -37,22 +37,31 @@ int main(int argc, const char * argv[]) {
 	
 	@autoreleasepool {
 		
-		NSEnumerator *enumerator = [CountUpToXEnumerator countUpTo:1000000];
+		NSEnumerator *enumerator = [CountUpToXEnumerator countUpTo:100000];
 		
 		@autoreleasepool {
-			enumerator = [enumerator.rac_sequence map:^id(id value) {
+			enumerator = [[enumerator.rac_sequence map:^id(id value) {
 				return value;
+			}] filter:^BOOL(id value) {
+				return YES;
 			}].objectEnumerator;
 		}
 		
 		NSUInteger sum = 0;
-		for (NSNumber *x in enumerator) {
-			sum += x.unsignedIntegerValue;
+//		for (NSNumber *x in enumerator) {
+//			sum += x.unsignedIntegerValue;
+//		}
+		
+		for (;;) {
+			@autoreleasepool {
+				NSNumber *x __attribute__((objc_precise_lifetime)) = [enumerator nextObject];
+				if (x == nil) break;
+				sum += x.unsignedIntegerValue;
+			}
 		}
 
 	}
 	
-	sleep(UINT32_MAX);
 	return 0;
 }
 
